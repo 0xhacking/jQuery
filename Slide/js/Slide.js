8dcbce7cce5673be02507f7bb77af0a6b9c32a6f
0xhacking <lenvens@gmail.com>
2015-06-05 13:37:17 +0800

    add timer

--- Slide/js/Slide.js
+++ Slide/js/Slide.js
@@ -7,8 +7,27 @@ $(document).ready(function(){
     var currSlide = $('.thumbs li').filter('.curr').index();
     var nextSlide = currSlide + 1;
     var fadeSpeed = 1000;
-	
 
+    startTimer();
+  //Start slides timer functions
+    function startTimer() {
+        timer = setInterval(function () {
+            //$('.slide-item').eq(currSlide).fadeOut(fadeSpeed);
+            $('.slide-item, .thumbs li').removeClass('curr');
+
+            var offset  =($(".slide-item").eq(nextSlide).width())*-1
+$(".slide-item").animate({
+			left: offset
+		}, "slow", function() {
+            $('.slide-item').eq(nextSlide).addClass('curr').css("left","0px").fadeIn(fadeSpeed);
+		})
+            $('.thumbs li').eq(nextSlide).addClass('curr');
+
+            currSlide = nextSlide;
+            nextSlide = currSlide + 1 < slideCount ? currSlide + 1 : 0;
+
+        }, 6000);
+    }
         currSlide=0;
     $('body').keydown(function (e) {
         var keyCode = e.keyCode;
